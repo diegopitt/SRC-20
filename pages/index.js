d9d729462e47731fc56df0b297cb65ce545f0573
unknown <diego.pittalugagmail.com>
2023-05-22 19:43:13 -0600

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,10 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
 import { useRouter } from 'next/router'
 import { useMediaQuery } from '@mui/material';
 
@@ -33,7 +37,7 @@ function Main(props) {
                 <Typography component="span" variant="h4" color="#eda803" gutterBottom>
                   BITCOIN STAMPS
                 </Typography>
-                <Typography sx={{ fontWeight: 500, mt:2 }} variant="h5" paragraph color="#c4cad6">
+                <Typography sx={{ fontWeight: 500, mt: 2 }} variant="h5" paragraph color="#c4cad6">
                   Unprunable UTXO Art, Because Sats Dont Exists
                 </Typography>
                 <Typography variant="subtitle1" color="#eda803">
@@ -43,11 +47,20 @@ function Main(props) {
             </Grid>
           </Grid>
         </Paper>
+        <Grid container direction="row" justifyContent="center" alignItems="center" sx={{ pb: 2 }}>
+          <Paper component="form" sx={{ p: '2px 4px', mt: 2, display: 'flex', alignItems: 'center', width: 400 }}>
+            <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search stamp, creator, TX, CPID" inputProps={{ 'aria-label': 'search google maps' }} />
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </Grid>
         <Grid container direction="row" justifyContent="center" alignItems="center">
           <Grid item xs={12} sm={10} sx={{ pb: 4 }}>
             <ImageList variant="standard" cols={matches ? 3 : 2} gap={26}>
               {stamps.map((item) => (
-                <ImageListItem onClick={() => {  }} key={item.img} sx={{ m: { xs: 0, md: 2 } }}>
+                <ImageListItem onClick={() => { }} key={item.img} sx={{ m: { xs: 0, md: 2 } }}>
                   <img style={{ minHeight: 240, maxHeight: 308 }} src={`${item.img}?w=248&fit=crop&auto=format`} srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
                   <ImageListItemBar sx={{ "& .MuiImageListItemBar-subtitle": (theme) => theme.typography.stampSubTitle, "& .MuiImageListItemBar-title": (theme) => theme.typography.stampTitle, background: 'rgba(0,0,0,0.86)' }} title={'Stamp:'} subtitle={'Author:'} />
                 </ImageListItem>
@@ -61,19 +74,19 @@ function Main(props) {
 }
 const stamps = [
   { link: '/tcardomomo', img: '/st1.png', title: 'CARDOMOMO', Species: 'Elattaria Cardomumum' },
-  { link: '/dienteleon', img: '/st2.png', title: 'DIENTE DE LEON', Species: 'Taraxacum Officinale'},
-  { link: '/damiana', img: '/st3.jpg', title: 'DAMIANA', Species: 'Turnera Diffusa'},
-  { link: '/coladecaballo', img: '/st4.webp', title: 'COLA DE CABALLO', Species: 'Equisetum Arvense'},
-  { link: '/chilcuague', img: '/st5.png', title: 'CHILCUAGUE', Species: 'Heliopsis Longipes'},
+  { link: '/dienteleon', img: '/st2.png', title: 'DIENTE DE LEON', Species: 'Taraxacum Officinale' },
+  { link: '/damiana', img: '/st3.jpg', title: 'DAMIANA', Species: 'Turnera Diffusa' },
+  { link: '/coladecaballo', img: '/st4.webp', title: 'COLA DE CABALLO', Species: 'Equisetum Arvense' },
+  { link: '/chilcuague', img: '/st5.png', title: 'CHILCUAGUE', Species: 'Heliopsis Longipes' },
   { link: '/canela', img: '/st6.jpg', title: 'CANELA', Species: 'Cinnamumum Ceylanicum' },
   { link: '/albahaca', img: '/st7.jpg', title: 'ALBAHACA', Species: 'Ocimun Basilicum' },
   { link: '/tcacao', img: '/st8.webp', title: 'CACAO', Species: 'Theobroma Cacao', },
   { link: '/jamaica', img: '/st9.png', title: 'JAMAICA', Species: 'Hibiscus Sabdariffa' },
-  { link: '/cuachalalate', img: '/st10.png', title: 'CUACHALALATE', Species: 'Amphipterygium Adstringens'},
+  { link: '/cuachalalate', img: '/st10.png', title: 'CUACHALALATE', Species: 'Amphipterygium Adstringens' },
   { link: '/reishi', img: '/st11.jpg', title: 'HONGO REISHI', Species: 'Ganoderma Lucidum' },
-  { link: '/gengibre', img: '/st12.png', title: 'GENGIBRE', Species: 'Zingiber Officinale'},
-  { link: '/chilchahua', img: '/st4.webp', title: 'CHILCHAHUA', Species: 'Tagetes Nelsonii Greenm'},
-  { link: '/cafeverde', img: '/st2.png', title: 'CAFE VERDE', Species: 'Coffea Arabica'},
+  { link: '/gengibre', img: '/st12.png', title: 'GENGIBRE', Species: 'Zingiber Officinale' },
+  { link: '/chilchahua', img: '/st4.webp', title: 'CHILCHAHUA', Species: 'Tagetes Nelsonii Greenm' },
+  { link: '/cafeverde', img: '/st2.png', title: 'CAFE VERDE', Species: 'Coffea Arabica' },
   { link: '/hinojo', img: '/st3.jpg', title: 'HINOJO', Species: 'Foeniculum Vulgare', },
   { link: '/cempasuchil', img: '/st7.jpg', title: 'CEMPASUCHIL', Species: 'Tagetes Erecta L' }
 ];
